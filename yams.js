d86c8e0f9f56dcd60a765e35d8c0c61bb5d064c3
Elie Leroy <elieleroy4@gmail.com>
2025-10-30 10:19:02 +0100

    yams

--- yams.js
+++ yams.js
@@ -3,4 +3,9 @@ const isGrandeSuite=r=>{
   const s=[...new Set(r)].sort((a,b)=>a-b).join('')
   return s==='12345'||s==='23456'
 }
-export const scoreRoll=r=>isGrandeSuite(r)?40:sum(r)
+const isYams=r=>r.every(v=>v===r[0])
+export const scoreRoll=r=>{
+  if(isYams(r))return 50
+  if(isGrandeSuite(r))return 40
+  return sum(r)
+}
